567a44134c8317058d0b860f2f0729c62412ab81
Melchor Tatlonghari <melchor.t@YCH.COM>
2016-06-07 14:43:40 +0800

    merged files

--- src/main/java/com/bus/cap/constants/Constant.java
+++ src/main/java/com/bus/cap/constants/Constant.java
@@ -2,7 +2,8 @@ package com.bus.cap.constants;
 
 public class Constant {
 	public static final String ENTITY_PACKAGE = "com.bus.cap.entities";
-	
+	public static final String FILE_PATH = "C:\\file_dump";
+
 	public static final String SUCCESS = "has been Sucessfully Saved!";
 	public static final String FAIL = "An error has occured during Save operation.";
 	public static final String UPDATE = "has been Sucessfully updated!";
